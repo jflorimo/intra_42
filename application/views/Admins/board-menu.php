3fcee112dbe9564e31c7895d1a6eb70b7dfdbb21
jflorimo <flojeanc@yahoo.fr>
2014-05-27 16:38:43 +0200

    modif menu Admins/board-menu.php

--- application/views/Admins/board-menu.php
+++ application/views/Admins/board-menu.php
@@ -2,4 +2,7 @@
 	<a href='<?php echo site_url("admins")?>/board_admins' class="link-board-menu">Admins</a>
 	<a href='<?php echo site_url("admins")?>/board_categories' class="link-board-menu">Categories</a>
 	<a href='<?php echo site_url("admins")?>/board_souscategories' class="link-board-menu">Sous Categories</a>
+	<a href='<?php echo site_url("admins")?>/board_semestres' class="link-board-menu">Semestres</a>
+	<a href='<?php echo site_url("admins")?>/board_modules' class="link-board-menu">Modules</a>
+	<a href='<?php echo site_url("admins")?>/board_activites' class="link-board-menu">Activités</a>
 </div>

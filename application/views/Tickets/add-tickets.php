67fa08e3a8a10a0189765379cc4cdc74f3d0082e
Norman SCHILLING <nschilli@e2r3p18.42.fr>
2014-06-03 15:15:45 +0200

    ~ modification css tickets & Tickets(complet)

--- application/views/Tickets/add-tickets.php
+++ application/views/Tickets/add-tickets.php
@@ -1,8 +1,10 @@
-<div id="title-add-thread">Add a Ticket</div>
+<div id="block-answer-ticket">
+<div id="title-add-answer">Add a Ticket</div>
 <?php	echo form_open('tickets/add_tickets', array("id"=>"form-connexion")); ?>
 	<label for="subject" id="label-subject-thread">Subject</label>
 	<input type="text" id="input-subject-thread" name="subject" />
 	<br/>
-	<textarea id="textarea-description-thread" name="description" placeholder="Write your message here..."></textarea>
-	<input type="submit" id="submit-add-thread" value="Add" />
+	<textarea id="textarea-answer-thread" name="description" placeholder="Write your message here..."></textarea>
+	<input type="submit" id="submit-add-ticket" value="Add" />
 <?php echo form_close(); ?>
+</div>
